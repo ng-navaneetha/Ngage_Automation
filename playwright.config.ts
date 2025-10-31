3a8721c498a4154393e5024e9305c2b83c1ef800
ng-navaneetha <navaneetha.b@ngenux.com>
2025-10-31 11:55:05 +0530

    removed allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: [
     ['html'],
     ['json', { outputFile: 'results.json' }],
-    ['allure-playwright'],
+    
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
